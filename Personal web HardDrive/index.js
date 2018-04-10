fcb0bee627e09105fefe8630903e679870b59672
SinaRezadad <38264509+SinaRezadad@users.noreply.github.com>
2018-04-11 07:20:05 +1000

    These Codes Are For My Own Personal Page
    Hi Guys,
    
    I have developed these codes just to extend my skills. For example, I managed to implement Angular functions. Please feel free to advise me about any cool features which you may think is an advantage to have it on my page as a portfolio.
    
    I am actively looking for a job and I would appreciate if anyone could help me out.

---
+++ Personal web HardDrive/index.js
@@ -0,0 +1,56 @@
+function btnSkillsFunc (){
+
+  var Name; var Age; var myForm;
+  myForm = document.getElementById("initials");
+  Name = myForm.elements["Name"].value;
+  Age = myForm.elements["Age"].value;
+  if (Name =="" && Age =="") {
+    alert('Please Insert Your Name And Age');
+  }
+
+  else if (Age == "") {
+    alert('You Must Insert Your Age');
+  }
+
+  else if (Name =="") {
+    alert('Please Insert Your Name');
+  }
+  else{
+    document.getElementById("theName").innerHTML = ' Hi ' +Name+ "You Are " +Age+"Years Old";
+    document.getElementById('divSkills').style.display="block";
+    document.body.style.backgroundColor ="red";
+    }
+  }
+
+$(document).ready(function(){
+    $("#languages").click(function(){
+        $("#myEducation").hide();
+        $("#English").toggle(1000).slideUp(1000).slideDown(1000);;
+        $("#Spanish").toggle("slow");
+        $("#Farsi").toggle(1000);
+        $("#myLanguages").fadeIn(1000);
+    });
+});
+
+$(document).ready(function(){
+    $("#education").click(function(){
+        $("#myLanguages").hide();
+        $("#myEducation").toggle(1000).slideUp(1000).slideDown(1000);;
+        $("#university").toggle(1000);
+        $("#MCSD").toggle(4000);
+        $("#Interpreter").fadeIn(1000);
+    });
+});
+
+function textReader(){
+  document.getElementById('textReaderDiv').style.display = "block";
+}
+
+function myDate(){
+  document.getElementById("myDate").innerHTML =   Date();
+}
+
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope) {
+    $scope.myText = "";
+});
